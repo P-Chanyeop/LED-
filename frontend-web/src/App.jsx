9be363d3b7ec0fa20b4dc078b6d4d823184267a2
P-Chanyeop <qkrcksduq1gh@gmail.com>
2026-03-04 16:37:06 +0900

    태블릿 페이지 별도 경로 추가 및 빌드

--- frontend-web/src/App.jsx
+++ frontend-web/src/App.jsx
@@ -60,6 +60,7 @@ function Layout() {
       <Routes>
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignupPage />} />
+        <Route path="/tablet" element={<TabletEstimateForm />} />
         <Route path="/" element={
           <PrivateRoute>
             {isTabletMode ? <TabletEstimateForm /> : <EstimateForm />}
